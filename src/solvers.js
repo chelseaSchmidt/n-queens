ef1f0f5f550b1d53b92ef9d9b9f4d71b20e89da2
Chelsea Schmidt <cschmidt331@gmail.com>
2020-05-21 13:16:17 -0700

    Implemented first attempt at findNRooksSolution (bug noted within)

--- src/solvers.js
+++ src/solvers.js
@@ -14,10 +14,45 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
+//j: given n find a single valid arrangement of n rooks on a nXn chessboard, and return that chessboard stringified -- which is an instancnce of Board
+  //for each value in the board a 0 represents a not occupied space, a 1 represents a rook
+//i: N, determines number of rooks and deimensions of board
+//o: return a board instance
+//s: creating an ew instance no side effects
+//c: perhaps quadratic - nested loops
+//e: if n is passed in invalid numbers, or 0, or n is too high, or junk char
+//exp: use n to instantiate a board - modify that board and test to find a working solution, if a solution - meaning all rooks have been placed with no conflicts - return the modified board
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
+  let solution = new Board({n: n}); //Bug here: solution should be a matrix, not a board instance!
+  //set up a out of bounds storage object or array
+  let outOfBoundsCol = [];
+  //set up a storage for inbounds columns as an array of valid column indices (1 to n-1)
+  let inBoundsCol = _.range(1, solution.get('n') - 1);
+  //place a rook at row zero column 0;
+  solution.togglePiece(0, 0);
+  //get grid with this rows method
+  let grid = solution.rows();
+  //starting at row index 1 for each remaining row...
+  for (var row = 1; row < grid.length; row++) {
+    //set up a min inbound array var
+    let minInboundsIndex;
+    //for each inbounds array
+    for (var index = 0; index < inBoundsCol.length; index++) {
+      //if the index of current inbounds value is -1 in outboudnds array
+      if (outOfBoundsCol.indexOf(inBoundsCol[index]) === -1) {
+              //set minInbounds value to current inbounds value
+          minInboundsIndex = inBoundsCol[index];
+        //break ouf the search for the min valid column index
+        break;
+      }
+    }
+    //toggle the value of current index at mininbounds column index to 1
+    solution.togglePiece(row, minInboundsIndex);
+    //update out of bounds storage with current column index
+    outOfBoundsCol.push(minInboundsIndex);
+  }
+  console.log(solution);
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
